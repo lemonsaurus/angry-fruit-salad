eaa8d1ca495b9127378a5ee51ad0725795578505
Derek <sjukledighet@gmail.com>
2021-06-12 11:44:40 -0400

    Add enemy and projectile scenes

---
+++ scenes/Enemies/Pepperoni Bullet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var move = Vector2.ZERO
+var look_vector = Vector2.ZERO
+var player = null
+var speed = 15
+
+
+func _ready():
+	look_vector = player.position - global_position
+	
+	
+func _physics_process(delta):
+	move = Vector2.ZERO
+	move = move.move_toward(look_vector, delta)
+	move = move.normalized() * speed
+	position += move
