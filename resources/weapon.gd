4e2bab9914dec506a50c7b6d239cc59d43b66f04
poffo <ltkpoffo@gmail.com>
2021-06-12 18:52:57 -0300

    feat: add weapon fusion

--- resources/weapon.gd
+++ resources/weapon.gd
@@ -5,3 +5,15 @@ export var name : String
 export var tooltip : String
 export var texture: Texture
 export var node: PackedScene
+
+enum ID {
+	LASER_BANANA,
+	GRAPESHOT,
+	PINETHROWER,
+	SUPER_BANANA,
+	BANANASHOT,
+	ANANAS
+}
+
+export(ID) var id
+
