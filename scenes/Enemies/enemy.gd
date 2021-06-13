e019f98cab994dc90457fde6d4b4c7cab4969cd6
Derek <sjukledighet@gmail.com>
2021-06-13 13:08:39 -0400

    Add patrol behavior
    fix player detection

--- scenes/Enemies/enemy.gd
+++ scenes/Enemies/enemy.gd
@@ -6,33 +6,44 @@ onready var map_nav = get_parent().get_node("Navigation2D")
 var player = null
 var velocity = Vector2.ZERO
 var path_to_dest = []
+var direction = null
 export var speed = 200
 
 
 func _physics_process(delta):
 	var move = Vector2.ZERO
-	
+
 	if player != null:
 		var enemy_pos = get_global_position()
 		var destination = map_nav.get_closest_point(player.position)
 		path_to_dest = map_nav.get_simple_path(enemy_pos, destination, false)
 
-	if path_to_dest.size() > 1:
-		move = position.direction_to(path_to_dest[1])  # first point is current position
+		if path_to_dest.size() > 1:
+			move = position.direction_to(path_to_dest[1])  # first point is current position
 	else:
-		move = Vector2.ZERO
+		var enemy_pos = get_global_position()
+		match direction:
+			0: enemy_pos.x += 1
+			1: enemy_pos.x -= 1
+			2: enemy_pos.y += 1
+			3: enemy_pos.y -= 1
+			_: enemy_pos.x += 1
+
+		move = position.direction_to(enemy_pos)
 
 	velocity = move.normalized()
-	velocity = move_and_slide(velocity*speed)
+	if velocity != Vector2.ZERO:
+		velocity = move_and_slide(velocity*speed)
 
 
 func _on_Area2D_body_entered(body):
-	if body != self:
+	if body.is_in_group("player"):
 		player = body
 
 
 func _on_Area2D_body_exited(body):
-	player = null
+	if body.is_in_group("player"):
+		player = null
 
 
 func fire():
@@ -46,3 +57,7 @@ func fire():
 func _on_Timer_timeout():
 	if player != null:
 		fire()
+
+
+func _on_Change_Direction_timeout():
+	direction = randi() % 4
